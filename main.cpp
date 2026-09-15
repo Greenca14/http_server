7fb6d919fd9e00eccec05872aa86b8c8f22f9cf0
Greenca14 <124871992+Greenca14@users.noreply.github.com>
2026-09-16 00:04:47 +0500

    Add ThreadPool for parallel client handling

--- main.cpp
+++ main.cpp
@@ -1,25 +1,27 @@
 #include "wsa_init.hpp"
 #include "socket.hpp"
+#include "thread_pool.hpp"
 #include <iostream>
 #include <chrono>
 #include <sstream>
 #include <cstring>
 #include <windows.h>
+#include <memory>
+
+static const std::string response = 
+	"HTTP/1.1 200 OK\r\n"
+	"Content-Type: text/plain\r\n"
+	"Content-Length: 13\r\n"
+	"Connection: close\r\n"
+	"\r\n"
+	"Hello!";
 
 int main() {
 	try {
 		WsaInit wsa;
-		
-		//std::string body = "Hello!";
-		std::string body(100000, 'X');
-		std::ostringstream resp;
-		resp << "HTTP/1.1 200 OK\r\n"
-			<< "Content-Type: text/plain\r\n"
-			<< "Content-Length: " << body.size() << "\r\n"
-			<< "Connection: close\r\n"
-			<< "\r\n"
-			<< body;
-		std::string response = resp.str();
+
+		ThreadPool pool(6);
+		std::cout << "Pool with 4 workers created\n";
 
 		Socket server(::socket(AF_INET, SOCK_STREAM, 0));
 		if (!server.valid()) {
@@ -39,18 +41,16 @@ int main() {
 			Socket client = server.accept_client();
 			std::cout << "Client connected\n";
 
-			//int sent = ::send(client.get(), response.data(), response.size(), 0);
-			//if (sent == SOCKET_ERROR) {
-			//	std::cerr << "send failed: " << WSAGetLastError() << "\n";
-			//}
-			try {
-				client.send_all(response);
-			}
-			catch (const std::exception& e) {
-				std::cerr << "send failed: " << e.what() << "\n";
-			}
-			::shutdown(client.get(), SD_SEND);
-			Sleep(100);
+			pool.enqueue([client = std::make_shared<Socket>(std::move(client))]() mutable {
+				try {
+					client->send_all(response);
+				}
+				catch (const std::exception& e) {
+					std::cerr << "send failed: " << e.what() << "\n";
+				}
+				::shutdown(client->get(), SD_SEND);
+				Sleep(100);
+			});
 		}
 	}
 	catch (const std::exception& e){
